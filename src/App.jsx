95dd464315693274387cac8ac7d3f8ad027a0854
InobatMi <inobatmirahmedova@gmail.com>
2025-04-20 22:35:06 +0500

    feat: all new things added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1 className="bg-pink-600">hello world</h1>
+      <h1 className="bg-pink-600 text-white">hello world</h1>
     </>
   );
 }
